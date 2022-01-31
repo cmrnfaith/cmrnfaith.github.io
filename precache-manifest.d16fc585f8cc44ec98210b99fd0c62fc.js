3bd119e3c5f0a36172851b57637f2dd77d846655
Cameron Faith <cmrnfaith@gmail.com>
2022-01-31 12:42:57 -0700

    Updates

---
+++ precache-manifest.d16fc585f8cc44ec98210b99fd0c62fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65df7c11274ec3b6dc1cb10fae479c48",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b2851628d1809a42633a",
+    "url": "/portfolio/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "d7749eda062063c7672f",
+    "url": "/portfolio/static/css/main.4cfe7c4b.chunk.css"
+  },
+  {
+    "revision": "b2851628d1809a42633a",
+    "url": "/portfolio/static/js/2.619f7387.chunk.js"
+  },
+  {
+    "revision": "a9807912173ef280ea3f",
+    "url": "/portfolio/static/js/3.9f01db21.chunk.js"
+  },
+  {
+    "revision": "d7749eda062063c7672f",
+    "url": "/portfolio/static/js/main.8b4b4208.chunk.js"
+  },
+  {
+    "revision": "dc4c4e3bab6c7f69d569",
+    "url": "/portfolio/static/js/runtime~main.cd79d7bc.js"
+  }
+]);
