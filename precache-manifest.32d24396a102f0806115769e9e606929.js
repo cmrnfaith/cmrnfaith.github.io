a37c4c65a0764a79ca73afbfb45ff4a5eec5364b
Cameron Faith <cmrnfaith@gmail.com>
2022-01-31 15:16:08 -0700

    Updates

---
+++ precache-manifest.32d24396a102f0806115769e9e606929.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3947a446c4546b5339a70ad1111906",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5bac47d13ce8e2cf6e37",
+    "url": "/portfolio/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "caf97b47576fc34bac6a",
+    "url": "/portfolio/static/css/main.f09ad4a8.chunk.css"
+  },
+  {
+    "revision": "5bac47d13ce8e2cf6e37",
+    "url": "/portfolio/static/js/2.2c16e59d.chunk.js"
+  },
+  {
+    "revision": "114a46de5c05ca60e778",
+    "url": "/portfolio/static/js/3.c7b6313b.chunk.js"
+  },
+  {
+    "revision": "caf97b47576fc34bac6a",
+    "url": "/portfolio/static/js/main.c8b6a952.chunk.js"
+  },
+  {
+    "revision": "f67e9ed8d8b20fac6a78",
+    "url": "/portfolio/static/js/runtime~main.c4c92ff8.js"
+  },
+  {
+    "revision": "e51ce23d2eb556f2b856f73cfe4d3ce8",
+    "url": "/portfolio/static/media/Cameron Faith - Resume.e51ce23d.pdf"
+  },
+  {
+    "revision": "b451ebf09f666d5e299981db77a22d04",
+    "url": "/portfolio/static/media/profile.b451ebf0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.11e284084d4709f7ce78f8c6c5aeac44.js"
+  "/portfolio/precache-manifest.32d24396a102f0806115769e9e606929.js"
 );
 
 self.addEventListener('message', (event) => {
