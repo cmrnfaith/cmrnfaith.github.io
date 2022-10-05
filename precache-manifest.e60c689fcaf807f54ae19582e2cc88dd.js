84559b054156c12ee6672e00d9e5daa4d6585bdd
cydro <cmrnfaith@gmail.com>
2022-10-04 20:32:04 -0600

    Updates

--- precache-manifest.e60c689fcaf807f54ae19582e2cc88dd.js
+++ precache-manifest.e60c689fcaf807f54ae19582e2cc88dd.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a6ccbfa7262afbcc290d18ca7cdee6d4",
+    "revision": "3610fd3f9fff77b16e10d11e0973357f",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "07a3035a0a9dca0553b1",
+    "revision": "e62acddc52fd8ec47553",
     "url": "/portfolio/static/css/2.68a81a89.chunk.css"
   },
   {
-    "revision": "4d2f61e19c19772c7b26",
+    "revision": "4c2c5bea53561cb2c1e3",
     "url": "/portfolio/static/css/main.516f50d3.chunk.css"
   },
   {
-    "revision": "07a3035a0a9dca0553b1",
-    "url": "/portfolio/static/js/2.a5dd851e.chunk.js"
+    "revision": "e62acddc52fd8ec47553",
+    "url": "/portfolio/static/js/2.10614998.chunk.js"
   },
   {
     "revision": "d14e4b2bc92b6ae455e9",
     "url": "/portfolio/static/js/3.4ea34601.chunk.js"
   },
   {
-    "revision": "4d2f61e19c19772c7b26",
-    "url": "/portfolio/static/js/main.f7455225.chunk.js"
+    "revision": "4c2c5bea53561cb2c1e3",
+    "url": "/portfolio/static/js/main.3884003b.chunk.js"
   },
   {
     "revision": "ccdf6b528165b22a6706",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4657ccb71b32a65127e8406a7a01f21b.js"
+  "/portfolio/precache-manifest.e60c689fcaf807f54ae19582e2cc88dd.js"
 );
 
 self.addEventListener('message', (event) => {
